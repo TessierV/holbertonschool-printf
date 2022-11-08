f302df685ddfce7731c0b1e2983b514fdefdbdc0
paulinepar <paulineparmigiani@gmail.com>
2022-11-08 10:32:40 +0100

    transfer

--- op_functions.c
+++ op_functions.c
@@ -1,43 +1,28 @@
 #include "main.h"
+
 /**
  *print_char - print char
  *@arguments: arguments
  */
-void print_char(va_list arguments)
+int print_char(va_list arguments)
 {
-	printf("%c", va_arg(arguments, int));
+	int c;
+
+	c = va_arg(arguments, int);
+	putchar(c);
+	return (0);
 }
 
 /**
- * print_int - integers
- * @arguments: arguments
+ *print_string - print string
+ *@arguments: arguments
  */
-void print_int(va_list arguments)
-{
-	printf("%d", va_arg(arguments, int));
-}
 
-/**
- * print_float - float
- * @arguments: arguments
- */
-void print_float(va_list arguments)
+int print_string(va_list arguments)
 {
-	printf("%f", va_arg(arguments, double));
-}
+	int s;
 
-/**
- * print_string - string
- * @arguments: arguments
- */
-void print_string(va_list arguments)
-{
-	char *args = va_arg(arguments, char*);
-
-	if (args == NULL)
-	{
-		printf("%p", args);
-		return;
-	}
-	printf("%s", args);
+	s = va_arg(arguments, int);
+	putchar(s);
+	return (0);
 }
